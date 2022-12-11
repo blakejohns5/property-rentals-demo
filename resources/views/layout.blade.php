639ed657888ae348426b0bc1014b334496090888
blakejohns5 <100588924+blakejohns5@users.noreply.github.com>
2022-12-11 11:43:36 +0100

    update blade files

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,11 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Vacation Rentals</title>
 </head>
 <body>
-  <h1>Vacation Rentals</h1>
+  <h1 class="bg-red-200">Vacation Rentals</h1>
   {{-- VIEW OUTPUT --}}
   @yield('content')
 </body>
--- resources/views/places.blade.php
+++ resources/views/places.blade.php
@@ -1,6 +1,4 @@
 @extends('layout')
-
-
 @section('content')
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
